aa5d4d2faadad8bb8a8f9d1086f151ebd96e770a
ViniciusZacheu <vnzacheu@gmail.com>
2021-10-25 21:01:46 -0300

    feat: added error validation to RegisterScreen.js

--- my-app/src/pages/login/LoginScreen.js
+++ my-app/src/pages/login/LoginScreen.js
@@ -1,39 +1,48 @@
-import '../login/LoginSceenStyle.css';
+import './LoginScreenStyle.module.css';
 import React, {useState} from "react";
-import {Button, Link, TextField} from "@material-ui/core";
+import {Button, Card, CardContent, Link, TextField} from "@material-ui/core";
 
 export default function LoginScreen() {
     const [userData, setUserData] = useState({username: "", password: ""})
     return (
-        <div className="main">
-            <div className="inputRow">
-                <TextField
-                    id="user-email"
-                    label="Email"
-                    placeholder="Insira seu email"
-                    variant="filled"
-                    type="email"
-                    onChange={e => setUserData({...userData, username: e.target.value})}
-                    value={userData.username}
-                />
-            </div>
-            <div className="inputRow">
-                <TextField
-                    id="user-password"
-                    label="Senha"
-                    placeholder="Insira sua senha"
-                    variant="filled"
-                    type="password"
-                    onChange={e => setUserData({...userData, password: e.target.value})}
-                    value={userData.password}
-                />
-            </div>
-            <div className="inputRow">
-                <Button onClick={() => console.log(userData)} variant="contained">Entrar</Button>
-            </div>
-            <div className="inputRow">
-                <span>Não tem conta? <Link href="/register">Cadastre-se!</Link></span>
-            </div>
+        <div className="mainLogin">
+            <Card sx={{ width: 700 }}>
+                <CardContent>
+                    <div className="pageTitle">
+                        <span className="titleText">Já tem uma conta?</span>
+                        <span>Insira seus dados abaixo e aproveite dos benefícios!</span>
+                        <div className="separator"/>
+                    </div>
+                    <div className="inputRow">
+                        <TextField
+                            id="user-email"
+                            label="Email"
+                            placeholder="Insira seu email"
+                            variant="outlined"
+                            type="email"
+                            onChange={e => setUserData({...userData, username: e.target.value})}
+                            value={userData.username}
+                        />
+                    </div>
+                    <div className="inputRow">
+                        <TextField
+                            id="user-password"
+                            label="Senha"
+                            placeholder="Insira sua senha"
+                            variant="outlined"
+                            type="password"
+                            onChange={e => setUserData({...userData, password: e.target.value})}
+                            value={userData.password}
+                        />
+                    </div>
+                    <div className="inputRow">
+                        <Button onClick={() => console.log(userData)} variant="contained">Entrar</Button>
+                    </div>
+                    <div className="inputRow">
+                        <span>Não tem conta? <Link href="/register">Cadastre-se!</Link></span>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     );
 }
--- my-app/src/pages/register/RegisterScreen.js
+++ my-app/src/pages/register/RegisterScreen.js
@@ -1,140 +1,276 @@
-import '../register/RegisterScreenStyle.css';
+import './RegisterScreenStyle.module.css';
 import React, {useState} from "react";
-import {Button, FormControl, IconButton, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
+import {
+    Button,
+    Card,
+    CardContent,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField,
+} from "@material-ui/core";
 import UserType from "../../enums/UserTypeEnum";
-import { Visibility } from '@material-ui/icons';
+import UserSex from "../../enums/UserSexEnum";
+import { Visibility, Check } from '@material-ui/icons';
+import {useHistory} from "react-router-dom";
+
+const INITIAL_STATE = {
+  value: "",
+  touched: false,
+  hasError: true,
+};
 
 export default function RegisterScreen() {
-    // ADICIONAR VALIDACAO DE HAS ERROR
-    const [userData, setUserData] = useState(
-        {
-            email: "",
-            password: "",
-            name: "",
-            age: null,
-            sex: "",
-            weight: null,
-            height: null,
-            userType: "",
-        }
-    )
+    let history = useHistory();
+    const [email, setEmail] = useState(INITIAL_STATE);
+    const [password, setPassword] = useState(INITIAL_STATE);
+    const [name, setName] = useState(INITIAL_STATE);
+    const [age, setAge] = useState(INITIAL_STATE);
+    const [sex, setSex] = useState(INITIAL_STATE);
+    const [weight, setWeight] = useState(INITIAL_STATE);
+    const [height, setHeight] = useState(INITIAL_STATE);
+    const [userType, setUserType] = useState(INITIAL_STATE);
+    const [setUserData] = useState({});
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+    const validateField = (value, field) => {
+        if (field === "email") {
+            return setEmail({
+                ...email,
+                touched: true,
+                hasError:
+                    value.length < 6 ||
+                    !value.includes("@")
+            });
+        }
+        if (field === "senha") {
+            return setPassword({
+                ...password,
+                touched: true,
+                hasError: value.length < 6
+            });
+        }
+        if (field === "nome") {
+            return setName({
+                ...name,
+                touched: true,
+                hasError: !(
+                    value.trim().length > 0 &&
+                    value.split(" ").length > 1 &&
+                    value.split(" ")[1] !== ""
+                )
+            });
+        }
+        if (field === "idade") {
+            return setAge({
+                ...age,
+                touched: true,
+                hasError: value < 0 || value > 120
+            });
+        }
+        if (field === "peso") {
+            return setWeight({
+                ...weight,
+                touched: true,
+                hasError: value < 0
+            });
+        }
+        if (field === "altura") {
+            return setHeight({
+                ...height,
+                touched: true,
+                hasError: value < 0 || value > 300
+            });
+        }
+        if (field === "sexo") {
+            return setSex({
+                ...sex,
+                touched: true,
+                hasError: value.length === 0
+            });
+        }
+        if (field === "classificacao") {
+            return setUserType({
+                ...userType,
+                touched: true,
+                hasError: value.length === 0
+            });
+        }
+    }
+
+    const submit = () => {
+        //FALTA INTEGRAÇÃO COM A API
+        setUserData({
+            email: email.value,
+            password: password.value,
+            name: name.value,
+            age: age.value,
+            sex: sex.value,
+            weight: weight.value,
+            height: height.value,
+            userType: userType.value
+        });
+        history.push("/login")
+    }
     return (
-        <div className="main">
-            <div className="inputRow">
-                <div className="inputWrapper">
-                    <TextField
-                        label="Email"
-                        placeholder="Insira seu email"
-                        variant="outlined"
-                        type="email"
-                        onChange={e => setUserData({...userData, email: e.target.value})}
-                        value={userData.email}
-                    />
-                </div>
-                <div className="inputWrapper">
-                    <div className="inputWithIcon">
-                        <TextField
-                            label="Senha"
-                            placeholder="Insira sua senha"
-                            variant="outlined"
-                            type={isPasswordVisible ? "text" : "password"}
-                            onChange={e => setUserData({...userData, password: e.target.value})}
-                            value={userData.password}
-                        />
-                        <div className="passwordVisibleIcon" onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
-                            <IconButton color="primary" component="span">
-                                <Visibility />
-                            </IconButton>
+        <div className="mainRegister">
+            <Card sx={{ width: 700 }}>
+                <CardContent>
+                    <div className="pageTitle">
+                        <span className="titleText">Crie uma conta</span>
+                        <span>Fácil e rápido.</span>
+                        <div className="separator"/>
+                    </div>
+                    <div className="inputRowWithIcon">
+                        <div className="inputWrapper">
+                            <TextField
+                                error={email.hasError && email.touched}
+                                label="Email"
+                                placeholder="Insira seu email"
+                                variant="outlined"
+                                type="email"
+                                onChange={e => setEmail({...email, value: e.target.value})}
+                                onBlur={e => validateField(e.target.value, "email")}
+                                value={email.value}
+                            />
+                        </div>
+                        <div className="inputWrapper">
+                            <div className="inputWithIcon">
+                                <TextField
+                                    error={password.hasError && password.touched}
+                                    label="Senha"
+                                    placeholder="Insira sua senha"
+                                    variant="outlined"
+                                    type={isPasswordVisible ? "text" : "password"}
+                                    onChange={e => setPassword({...password, value: e.target.value})}
+                                    onBlur={e => validateField(e.target.value, "senha")}
+                                    value={password.value}
+                                />
+                                <div className="passwordVisibleIcon" onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+                                    <IconButton color="primary" component="span">
+                                        <Visibility />
+                                    </IconButton>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="inputRow">
-                <div className="inputWrapper">
-                    <TextField
-                        label="Nome"
-                        placeholder="Insira seu nome completo"
-                        variant="outlined"
-                        type="text"
-                        onChange={e => setUserData({...userData, name: e.target.value})}
-                        value={userData.name}
-                    />
-                </div>
-                <div className="inputWrapper">
-                    <TextField
-                        label="Idade"
-                        placeholder="Insira sua idade"
-                        variant="outlined"
-                        type="number"
-                        onChange={e => setUserData({...userData, age: e.target.value})}
-                        value={userData.age}
-                    />
-                </div>
-            </div>
-            <div className="inputRow">
-                <div className="inputWrapper">
-                    <div className="inputWrapper">
-                        <TextField
-                            label="Peso"
-                            placeholder="Insira seu Peso"
-                            variant="outlined"
-                            type="number"
-                            onChange={e => setUserData({...userData, weight: e.target.value})}
-                            value={userData.weight}
-                        />
+                    <div className="inputRow">
+                        <div className="inputWrapper">
+                            <TextField
+                                error={name.hasError && name.touched}
+                                label="Nome"
+                                placeholder="Insira seu nome completo"
+                                variant="outlined"
+                                type="text"
+                                onChange={e => setName({...name, value: e.target.value})}
+                                onBlur={e => validateField(e.target.value, "nome")}
+                                value={name.value}
+                            />
+                        </div>
+                        <div className="inputWrapper">
+                            <TextField
+                                error={age.hasError && age.touched}
+                                label="Idade"
+                                placeholder="Insira sua idade"
+                                variant="outlined"
+                                type="number"
+                                onChange={e => setAge({...age, value: e.target.value})}
+                                onBlur={e => validateField(e.target.value, "idade")}
+                                value={age.value}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="selectInputWrapper">
-                    <FormControl sx={{ minWidth: 195 }}>
-                        <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={userData.sex}
-                            label="Sexo"
-                            onChange={e => setUserData({...userData, sex: e.target.value})}
-                        >
-                            <MenuItem value={"masc"}>Masculino</MenuItem>
-                            <MenuItem value={"fem"}>Feminino</MenuItem>
-                            <MenuItem value={"nenhum"}>Prefiro não informar</MenuItem>
-                        </Select>
-                    </FormControl>
-                </div>
-            </div>
-            <div className="inputRow">
-                <div className="inputWrapper">
-                    <div className="inputWrapper">
-                        <TextField
-                            label="Altura"
-                            placeholder="Insira sua Altura"
-                            variant="outlined"
-                            type="number"
-                            onChange={e => setUserData({...userData, height: e.target.value})}
-                            value={userData.height}
-                        />
+                    <div className="inputRow">
+                        <div className="inputWrapper">
+                            <div className="inputWrapper">
+                                <TextField
+                                    error={weight.hasError && weight.touched}
+                                    label="Peso (kg)"
+                                    placeholder="Insira seu Peso"
+                                    variant="outlined"
+                                    type="number"
+                                    onChange={e => setWeight({...weight, value: e.target.value})}
+                                    onBlur={e => validateField(e.target.value, "peso")}
+                                    value={weight.value}
+                                />
+                            </div>
+                        </div>
+                        <div className="selectInputWrapper">
+                            <FormControl sx={{ minWidth: 195 }}>
+                                <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
+                                <Select
+                                    error={sex.hasError && sex.touched}
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={sex.value}
+                                    label="Sexo"
+                                    onChange={e => setSex({...sex, value: e.target.value})}
+                                    onBlur={e => validateField(e.target.value, "sexo")}
+                                >
+                                    <MenuItem value={UserSex.MASCULINO}>Masculino</MenuItem>
+                                    <MenuItem value={UserSex.FEMININO}>Feminino</MenuItem>
+                                    <MenuItem value={UserSex.NAO_DECLARADO}>Prefiro não informar</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
                     </div>
-                </div>
-                <div className="selectInputWrapper">
-                    <FormControl sx={{ minWidth: 195 }}>
-                        <InputLabel id="demo-simple-select-label">Classificação</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={userData.userType}
-                            label="Classificação"
-                            onChange={e => setUserData({...userData, userType: e.target.value})}
+                    <div className="inputRow">
+                        <div className="inputWrapper">
+                            <div className="inputWrapper">
+                                <TextField
+                                    error={height.hasError && height.touched}
+                                    label="Altura (cm)"
+                                    placeholder="Insira sua Altura"
+                                    variant="outlined"
+                                    type="number"
+                                    onChange={e => setHeight({...height, value: e.target.value})}
+                                    onBlur={e => validateField(e.target.value, "altura")}
+                                    value={height.value}
+                                />
+                            </div>
+                        </div>
+                        <div className="selectInputWrapper">
+                            <FormControl sx={{ minWidth: 195 }}>
+                                <InputLabel id="demo-simple-select-label">Classificação</InputLabel>
+                                <Select
+                                    error={userType.hasError && userType.touched}
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={userType.value}
+                                    label="Classificação"
+                                    onChange={e => setUserType({...userType, value: e.target.value})}
+                                    onBlur={e => validateField(e.target.value, "classificacao")}
+                                >
+                                    <MenuItem value={UserType.FISICULTURISTA}>Fisiculturista</MenuItem>
+                                    <MenuItem value={UserType.PADRAO}>Frequentador</MenuItem>
+                                    <MenuItem value={UserType.NUTRICIONISTA}>Nutricionista</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </div>
+                    <div className="inputRow">
+                        <Button
+                            disabled={
+                                email.hasError ||
+                                password.hasError ||
+                                name.hasError ||
+                                age.hasError ||
+                                sex.hasError ||
+                                weight.hasError ||
+                                height.hasError ||
+                                userType.hasError
+                            }
+                            onClick={() => submit()}
+                            variant="contained"
+                            endIcon={<Check />}
                         >
-                            <MenuItem value={UserType.FISICULTURISTA}>Fisiculturista</MenuItem>
-                            <MenuItem value={UserType.PADRAO}>Frequentador</MenuItem>
-                            <MenuItem value={UserType.NUTRICIONISTA}>Nutricionista</MenuItem>
-                        </Select>
-                    </FormControl>
-                </div>
-            </div>
-            <div className="inputRow">
-                <Button onClick={() => console.log(userData)} variant="contained">Registrar</Button>
-            </div>
+                            Registrar
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
         </div>
     );
 }
