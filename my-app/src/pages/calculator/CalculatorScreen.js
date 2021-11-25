5f0ccb39458b146892ed0308050cf0ef287c4a15
ViniciusZacheu <vnzacheu@gmail.com>
2021-11-24 22:42:14 -0300

    feat: added first version of CalculatorScreen

--- my-app/src/pages/calculator/CalculatorScreen.js
+++ my-app/src/pages/calculator/CalculatorScreen.js
@@ -3,147 +3,111 @@ import React, {useState} from "react";
 import {
     Button,
     Card,
-    CardContent,
-    FormControl,
-    IconButton,
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
+    CardContent, FormControl, InputLabel, MenuItem, Select,
+    TextField
 } from "@material-ui/core";
-import ApiService from "../../api/ApiService"
-import UserSex from "../../enums/UserSexEnum";
+import {Add, DeleteForever} from "@material-ui/icons";
 
-const INITIAL_STATE = {
-    value: "",
-    touched: false,
-    hasError: true,
-};
+const INGREDIENTES_API = {
+    carboidratos: [
+        {value: 'batata_doce', label: 'Batata Doce'},
+        {value: 'mandioquinha', label: 'Mandioquinha'},
+        {value: 'batata', label: 'Batata'},
+        {value: 'arroz', label: 'Arroz'},
+    ],
+    legumes: [
+        {value: 'vagem', label: 'Vagem'},
+        {value: 'espinafre', label: 'Espinafre'},
+        {value: 'cenoura', label: 'Cenoura'},
+    ],
+    proteinas: [
+        {value: 'frango', label: 'Frango'},
+        {value: 'carne_de_boi', label: 'Carne de Boi'},
+        {value: 'carne_de_porco', label: 'Carne de Porco'},
+    ],
+}
 
 export default function CalculatorScreen() {
-    const apiInstance = ApiService();
-    const [carbo, setCarbo] = useState(INITIAL_STATE);
-    const [legume, setLegume] = useState(INITIAL_STATE);
-    const [proteina, setProteina] = useState(INITIAL_STATE);
+    const [inputList, setInputList] = useState([{ ingredient: "", portionQtd: "" }]);
 
-    const INGREDIENTES_API = {
-        carboidratos: [ 
-            {value: 'batata_doce', label: 'Batata Doce'},
-            {value: 'mandioquinha', label: 'Mandioquinha'},
-            {value: 'batata', label: 'Batata'},
-            {value: 'arroz', label: 'Arroz'},
-        ],
-        legumes: [
-            {value: 'vagem', label: 'Vagem'},
-            {value: 'espinafre', label: 'Espinafre'},
-            {value: 'cenoura', label: 'Cenoura'},
-        ],
-        proteinas: [
-            {value: 'frango', label: 'Frango'},
-            {value: 'carne_de_boi', label: 'Carne de Boi'},
-            {value: 'carne_de_porco', label: 'Carne de Porco'},
-        ],
-    }
+    const handleInputChange = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...inputList];
+        list[index][name] = value;
+        setInputList(list);
+    };
 
-    const validateField = (value, field) => {
-        if (field === "carbo") {
-            return setCarbo({
-                ...carbo,
-                touched: true,
-                hasError: value.length === 0,
-            });
-        }
-        if (field === "legume") {
-            return setLegume({
-                ...legume,
-                touched: true,
-                hasError: value.length === 0,
-            });
-        }
-        if (field === "proteina") {
-            return setProteina({
-                ...proteina,
-                touched: true,
-                hasError: value.length === 0,
-            });
-        }
-    }
+    const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+    };
 
-    const submit = async () => {
-        const result = await apiInstance.userRegister({
-            carbo: carbo.value,
-            legume: legume.value,
-            proteina: proteina.value,
-        });
-        console.log(result);
-    }
+    const handleAddClick = () => {
+        setInputList([...inputList, { ingredient: "", portionQtd: "" }]);
+    };
 
     return (
         <div className="calculatorMain">
-            <Card sx={{ width: 500 }}>
+            <Card sx={{ width: 1000 }}>
                 <CardContent>
-                    <div className="selectInputWrapper">
-                        <FormControl sx={{ minWidth: 195 }}>
-                            <InputLabel id="demo-simple-select-label">Carboidrato</InputLabel>
-                            <Select
-                                error={carbo.hasError && carbo.touched}
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={carbo.value}
-                                label="Carboidrato"
-                                onChange={e => setCarbo({
-                                    ...carbo,
-                                    value: e.target.value
-                                })}
-                                onBlur={e => validateField(e.target.value, "Carboidrato")}
-                            >
-                                { INGREDIENTES_API.carboidratos.map((item,idx) => (
-                                    <MenuItem key={idx} value={ item.value }>{ item.label }</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
+                    <div className="pageTitle">
+                        <span className="titleText">Calculadora Nutricional</span>
+                        <span>Cadastre um prato e verfique todas as informações nutricionais necessárias!</span>
+                        <div className="separator"/>
                     </div>
-                    <div className="selectInputWrapper">
-                        <FormControl sx={{ minWidth: 195 }}>
-                            <InputLabel id="demo-simple-select-label">Legume</InputLabel>
-                            <Select
-                                error={legume.hasError && legume.touched}
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={legume.value}
-                                label="Legumes"
-                                onChange={e => setLegume({
-                                    ...legume,
-                                    value: e.target.value
-                                })}
-                                onBlur={e => validateField(e.target.value, "Legume")}
-                            >
-                                { INGREDIENTES_API.legumes.map((item,idx) => (
-                                    <MenuItem key={idx} value={ item.value }>{ item.label }</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </div>
-                    <div className="selectInputWrapper">
-                        <FormControl sx={{ minWidth: 195 }}>
-                            <InputLabel id="demo-simple-select-label">Proteínas</InputLabel>
-                            <Select
-                                error={proteina.hasError && proteina.touched}
-                                labelId="demo-simple-select-label"
-                                id="demo-simple-select"
-                                value={proteina.value}
-                                label="Proteína"
-                                onChange={e => setProteina({
-                                    ...proteina,
-                                    value: e.target.value
-                                })}
-                                onBlur={e => validateField(e.target.value, "Proteína")}
-                            >
-                                { INGREDIENTES_API.proteinas.map((item,idx) => (
-                                    <MenuItem key={idx} value={ item.value }>{ item.label }</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
+                    {inputList.map((item, idx) => {
+                        return (
+                            <div key={idx} className="inputRow">
+                                <div className="calculatorInputMargin">
+                                    <FormControl sx={{ minWidth: 195 }}>
+                                        <InputLabel id={"select-" + idx}>Carboidrato</InputLabel>
+                                        <Select
+                                            name="ingredient"
+                                            labelId={"select-" + idx}
+                                            value={item.ingredient}
+                                            label="Carboidrato"
+                                            onChange={e => handleInputChange(e, idx)}
+                                        >
+                                            { INGREDIENTES_API.carboidratos.map((item,idx) => (
+                                                <MenuItem key={idx} value={ item.value }>{ item.label }</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                </div>
+                                <div className="calculatorInputMargin">
+                                    <TextField
+                                        name="portionQtd"
+                                        type="number"
+                                        label="Qtd de porções"
+                                        placeholder="Ex.: 2"
+                                        value={item.portionQtd}
+                                        onChange={e => handleInputChange(e, idx)}
+                                    />
+                                </div>
+                                <div className="calculatorInputMargin">
+                                    {inputList.length !== 0 &&
+                                        <Button
+                                            onClick={() => handleRemoveClick(item)}
+                                            variant="contained"
+                                            color="error"
+                                            endIcon={<DeleteForever />}>
+                                            Remover
+                                        </Button>
+                                    }
+                                </div>
+                            </div>
+                        );
+                    })}
+                    <div className="inputRow">
+                        <div className="calculatorInputMargin">
+                            <Button
+                                onClick={handleAddClick}
+                                variant="contained"
+                                endIcon={<Add />}>
+                                Adicionar novo campo
+                            </Button>
+                        </div>
                     </div>
                 </CardContent>
             </Card>
