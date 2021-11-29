53467cdb3fce08abe31868e00cdd0b6e049b5589
zizima100 <mr.zizima@gmail.com>
2021-11-29 15:53:22 -0300

    fix: About Font Size

--- my-app/src/pages/about/AboutScreen.js
+++ my-app/src/pages/about/AboutScreen.js
@@ -11,7 +11,7 @@ export default function AboutScreen() {
                         <span className="titleText">Sobre o Projeto</span>
                         <div className="separator"/>
                     </div>
-                    <span> O projeto PerfectPlate é um projeto de faculdade com a proposta de promover um maior controle nutricional dos pratos ingeridos por atletas e frequentadores de academia e, também, estimular um maior interesse e conscientização pela leitura de tabelas nutricionais. Como um bônus, ainda, o aplicativo possibilita um melhor e mais ativo relacionamento entre nutricionista e cliente ao oferecer uma ferramenta que pode ser utilizada em conjunto pelos dois.  </span>
+                    <p className="about__content"> O projeto PerfectPlate é um projeto de faculdade com a proposta de promover um maior controle nutricional dos pratos ingeridos por atletas e frequentadores de academia e, também, estimular um maior interesse e conscientização pela leitura de tabelas nutricionais. Como um bônus, ainda, o aplicativo possibilita um melhor e mais ativo relacionamento entre nutricionista e cliente ao oferecer uma ferramenta que pode ser utilizada em conjunto pelos dois.  </p>
                 </Card>
                 <Card className="about__card">
                     <div className="about__container-title">
