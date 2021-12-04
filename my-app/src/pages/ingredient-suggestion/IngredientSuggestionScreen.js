fa7e5889cdcb2a065581bd50a6456de9721c6295
Julio Faundes <mr.zizima@gmail.com>
2021-12-04 18:06:41 -0300

    feat: Ingredient Suggestion Validation

--- my-app/src/pages/ingredient-suggestion/IngredientSuggestionScreen.js
+++ my-app/src/pages/ingredient-suggestion/IngredientSuggestionScreen.js
@@ -1,21 +1,24 @@
 import './IngredientSuggestionScreenStyle.css';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
     Button,
     Card,
     CardContent,
     TextField
 } from "@material-ui/core";
-import {Check} from "@material-ui/icons";
+import { Check } from "@material-ui/icons";
 
 export default function IngredientSuggestionScreen() {
     const [ingredientName, setIngredientName] = useState("");
+
+    const hasNumber = /\d/;
 
     const submitIngredient = () => {
         const data = {
             ingredientName
         }
         console.log(data);
+        // console.log(ingredientName);
     }
 
     return (
@@ -25,7 +28,7 @@ export default function IngredientSuggestionScreen() {
                     <div className="pageTitle">
                         <span className="titleText">Sugestão de Ingredientes</span>
                         <span>Sugira um ingrediente que você está sentindo falta em nosso site!</span>
-                        <div className="separator"/>
+                        <div className="separator" />
                     </div>
                     <div className="inputRow">
                         <div className="calculatorInputMargin">
@@ -39,10 +42,14 @@ export default function IngredientSuggestionScreen() {
                             />
                         </div>
                     </div>
-                    <div className="separator"/>
+                    <div className="separator" />
                     <div className="inputRow">
                         <div className="calculatorInputMargin">
                             <Button
+                                disabled={
+                                    ingredientName === '' ||
+                                    hasNumber.test(ingredientName)
+                                }
                                 onClick={submitIngredient}
                                 color="success"
                                 variant="contained"
